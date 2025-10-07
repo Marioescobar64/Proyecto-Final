212b19c057689b7741cfff69a5b4382e4974fdc3
Marioescobar64 <mescobar-2022471@kinal.edu.gt>
2025-10-06 20:04:39 -0600

    Creacion del proyecto

---
+++ src/main/java/org/maridie/proyectoFinal/ProyectoFinalApplication.java
@@ -0,0 +1,15 @@
+package org.maridie.proyectoFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoFinalApplication.class, args);
+        System.out.println("Bienvenido al proyecto");
+
+    }
+}
+
