225ba8fe5cc95fb32eb343bb74be96a5ae854050
DAMB1828 <dmauricio-2022464@kinal.edu.gt>
2025-10-07 09:55:33 -0600

    Se creo la clase tipoDeSangreDto

--- src/main/java/org/maridie/proyectoFinal/dominio/dto/tipoDeSangreDto.java
+++ src/main/java/org/maridie/proyectoFinal/dominio/dto/tipoDeSangreDto.java
@@ -1,4 +1,19 @@
 package org.maridie.proyectoFinal.dominio.dto;
 
-public record tipoDeSangreDto() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class tipoDeSangreDto {
+    @PositiveOrZero
+    Integer id_tipo_sangre;
+    @NotBlank(message = "El grupo sanguineo es obligatorio")
+    String grupo_sanguineo;
+    @NotBlank(message = "El factor rh es obligatorio")
+    String factor_rh;
 }
