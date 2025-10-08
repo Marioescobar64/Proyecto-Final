28480f6816e90b56e46608d0f07a812c31ba1a6f
Marioescobar64 <mescobar-2022471@kinal.edu.gt>
2025-10-07 21:04:39 -0600

    aun no terminado

--- src/main/java/org/maridie/proyectoFinal/dominio/service/citaService.java
+++ src/main/java/org/maridie/proyectoFinal/dominio/service/citaService.java
@@ -1,4 +1,29 @@
 package org.maridie.proyectoFinal.dominio.service;
 
+import lombok.Data;
+import org.maridie.proyectoFinal.dominio.dto.*;
+import org.maridie.proyectoFinal.dominio.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Data
 public class citaService {
+    private final CitaRepository citaRepository;
+    private final DonadorRepository donadorRepository;
+    private final CentroRepository centroRepository;
+    private final JornadaRepository jornadaRepository;
+
+    public List<citaDto>  obtenerTodo() { return this.citaRepository.obtenerTodo();}
+    public citaDto buscarPorId(Integer id) { return this.citaRepository.buscarPorId(id);}
+    public citaDto guardar(citaDto cita) { return this.citaRepository.guardar(cita);}
+    public void eliminar(Integer id) {this.citaRepository.eliminar(id);}
+
+    public List<donadorDto> obtenerDonador() { return donadorRepository.obtenerTodo(); }
+    public List<centroDto> obtenerCentro() { return centroRepository.obtenerTodo(); }
+    public List<jornadaDto> obtenerJornadas() { return jornadaRepository.obtenerTodo(); }
+
+    public List<citaDto> buscarPorIdDonador(Integer id_donador) {return citaRepository.buscarPorIdDonador(id_donador);}
 }
