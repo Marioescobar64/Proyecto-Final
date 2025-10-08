9f8aa2832f266b43411c3f478d7ebf9b938912ae
DAMB1828 <dmauricio-2022464@kinal.edu.gt>
2025-10-07 19:53:56 -0600

    T14-Manejo de dominio de donacion

---
+++ src/main/java/org/maridie/proyectoFinal/repository/donacionRepository.java
@@ -0,0 +1,12 @@
+package org.maridie.proyectoFinal.repository;
+
+import org.maridie.proyectoFinal.dominio.dto.donacionDto;
+
+import java.util.List;
+
+public interface donacionRepository {
+    List<donacionDto> obtenerTodo();
+    donacionDto buscarPorId(Integer id);
+    donacionDto guardar(donacionDto donacion);
+    void eliminar(Integer id);
+}
