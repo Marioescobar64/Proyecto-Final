13eff692777d810d65276790f31f9faaf99f0af8
Marioescobar64 <mescobar-2022471@kinal.edu.gt>
2025-10-07 22:08:48 -0600

    T2, T16, T24 Ya realizada

---
+++ src/main/java/org/maridie/proyectoFinal/persistence/CitaEntityRepository.java
@@ -0,0 +1,61 @@
+package org.maridie.proyectoFinal.persistence;
+
+import org.maridie.proyectoFinal.dominio.dto.citaDto;
+import org.maridie.proyectoFinal.dominio.repository.CitaRepository;
+import org.maridie.proyectoFinal.persistence.crud.CrudCitaRepository;
+import org.maridie.proyectoFinal.persistence.entity.citaEntitty;
+import org.maridie.proyectoFinal.persistence.mapper.citaMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CitaEntityRepository implements CitaRepository {
+    private final CrudCitaRepository crudCitaRepository;
+    private final citaMapper citaMapper;
+
+    public CitaEntityRepository(CrudCitaRepository crudCitaRepository, citaMapper citaMapper) {
+        this.crudCitaRepository = crudCitaRepository;
+        this.citaMapper = citaMapper;
+    }
+
+    @Override
+    public List<citaDto> obtenerTodo() {
+        return this.citaMapper.toDto(this.crudCitaRepository.findAll());
+    }
+
+    @Override
+    public citaDto buscarPorId(Integer id) {
+        Optional<citaEntitty> cita = this.crudCitaRepository.findById(id);
+        return cita.map(citaMapper::toDto).orElse(null);
+    }
+    @Override
+    public List<citaDto> buscarPorIdDonador(Integer id_donador) {
+        List<citaEntitty> citas = crudCitaRepository.findByVDonador_id_donador(id_donador);
+        return citaMapper.toDto(citas);
+    }
+
+    @Override
+    public List<citaDto> buscarPorIdCentro(Integer id_centro) {
+        List<citaEntitty> citas = crudCitaRepository.findByVCentro_id_centro(id_centro);
+        return citaMapper.toDto(citas);
+    }
+
+    @Override
+    public List<citaDto> buscarPorIdJornada(Integer idInfractor) {
+        List<citaEntitty> citas = crudCitaRepository.findByJornada_id_jornada(idInfractor);
+        return citaMapper.toDto(citas);
+    }
+
+    @Override
+    public citaDto guardar(citaDto citaDto) {
+        citaEntitty cita = this.citaMapper.toEntity(citaDto);
+        return this.citaMapper.toDto(this.crudCitaRepository.save(cita));
+    }
+
+    @Override
+    public void eliminar(Integer id) {
+        this.crudCitaRepository.deleteById(id);
+    }
+}
--- src/main/java/org/maridie/proyectoFinal/persistence/crud/CrudCitaRepository.java
+++ src/main/java/org/maridie/proyectoFinal/persistence/crud/CrudCitaRepository.java
@@ -3,5 +3,10 @@ package org.maridie.proyectoFinal.persistence.crud;
 import org.maridie.proyectoFinal.persistence.entity.citaEntitty;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CrudCitaRepository extends CrudRepository<citaEntitty,Integer> {
+    List<citaEntitty> findByVDonador_id_donador(Integer id_donador);
+    List<citaEntitty> findByVCentro_id_centro(Integer id_centro);
+    List<citaEntitty> findByJornada_id_jornada(Integer id_jornada);
 }
